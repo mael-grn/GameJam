9645fb08447440837475509eb1d877d2a983aec6
Chloé <chloe.despesse@etu.univ-grenoble-alpes.fr>
2023-11-06 17:29:02 +0100

    ldfd

--- assets/img/Interior.tsx
+++ assets/img/Interior.tsx
@@ -1,4 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.2" name="Interior" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <grid orientation="orthogonal" width="64" height="32"/>
  <image source="../../../../Downloads/Interior.png" width="1024" height="1024"/>
 </tileset>
--- assets/img/couleur.tsx
+++ assets/img/couleur.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.2" name="couleur" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="../../../../Downloads/couleur.png" trans="ff00ff" width="96" height="96"/>
+ <image source="../../../../Downloads/couleur.png" width="96" height="96"/>
 </tileset>
