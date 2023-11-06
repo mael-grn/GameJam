f8416e11751bdc325ad68aa1b573ede5b0b113ed
maelg <mael.garnier@univ-grenoble-alpes.fr>
2023-11-06 11:41:38 +0100

    ajout nouveau parametre

--- screens/error.py
+++ screens/error.py
@@ -1,4 +1,11 @@
 import pygame
 
-def error(screen) : 
+def error(screen, error_name = "unknown error") : 
     screen.fill("black")
+    # Charger la police de caractères
+    police = pygame.font.Font(None, 30)
+    # Créer le texte
+    titre = police.render(error_name, True, (255, 255, 255))
+
+    # Afficher le texte
+    screen.blit(titre, (100, 100))
