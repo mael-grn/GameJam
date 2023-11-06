9cda10706bcb21c788a76d283ac7713f6d2ff3a0
maelg <mael.garnier@univ-grenoble-alpes.fr>
2023-11-06 17:38:55 +0100

    correction bug

--- assets/img/couleur.tsx
+++ assets/img/couleur.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="couleur" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="../../../../Downloads/couleur.png" width="96" height="96"/>
+<tileset version="1.10" tiledversion="1.10.2" name="couleur" tilewidth="32" tileheight="32" tilecount="9" columns="3">
+ <image source="couleur.png" width="96" height="96"/>
 </tileset>
