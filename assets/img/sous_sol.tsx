2b60ffb6d88fe84e96c00a0c58a6b4e50f9f0975
Chloé <chloe.despesse@etu.univ-grenoble-alpes.fr>
2023-11-07 09:30:43 +0100

    correction bug couloir 2

---
+++ assets/img/sous_sol.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="sous_sol" tilewidth="32" tileheight="32" tilecount="6" columns="2">
+ <image source="../../../../Downloads/sous_sol.png" width="64" height="96"/>
+</tileset>
