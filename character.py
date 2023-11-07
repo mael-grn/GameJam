231fe6f32e667fa49d4f09634da85ac752295e55
Yoann <yoann.goumarre@etu.univ-grenoble-alpes.fr>
2023-11-07 17:02:33 +0100

    Tout bien sauf personnage se fait encore os

--- character.py
+++ character.py
@@ -3,8 +3,8 @@ import projectile
 import game_logic
 import constants
 
-#un personnage dispose de codes couleurs par rapport à la carte : il ne peut pas passer sur la couleur noire,
-#et la couleur bleu lui fait changer de salle
+# Un personnage dispose de codes couleurs par rapport à la carte : il ne peut pas passer sur la couleur noire,
+# et la couleur bleu lui fait changer de salle
 
 class Character:
     def __init__(self, x, y):
@@ -28,7 +28,9 @@ class Character:
         self.direction = 0  # 0 pour droite, 1 pour gauche
         self.walk_step = 0  # 0 pour image 1, 1 pour image 2
         self.last_image_time = pygame.time.get_ticks()  # Temps de la dernière image
-    
+        self.hp = 3  # Initialisez les HP à leur valeur maximale
+        self.hp_timer = 0
+
     def move_left(self):
         self.rect.x -= self.speed
         self.direction = 1  # Gauche
@@ -36,7 +38,7 @@ class Character:
         if current_time - self.last_image_time > constants.ANIMATION_SPEED:  # Change d'image toutes les 0,5 secondes
             self.walk_step = 1 - self.walk_step
             self.last_image_time = current_time
-    
+
     def move_right(self):
         self.rect.x += self.speed
         self.direction = 0  # Droite
@@ -53,23 +55,24 @@ class Character:
             self.last_image_time = current_time
 
     def move_down(self):
-        self.rect.y += self.speed  
+        self.rect.y += self.speed
         current_time = pygame.time.get_ticks()
         if current_time - self.last_image_time > constants.ANIMATION_SPEED:  # Change d'image toutes les 0,5 secondes
             self.walk_step = 1 - self.walk_step
             self.last_image_time = current_time
 
-    def get_rect(self): #left high
+    def get_rect(self):  # left high
         return self.rect
-    
+
     def get_x(self):
         return self.rect.x
-    
+
     def get_y(self):
         return self.rect.y
 
     def get_centre_x(self):
         return self.rect.centerx
+
     def get_centre_y(self):
         return self.rect.centery
 
@@ -77,3 +80,33 @@ class Character:
         # Récupère l'image en fonction de la direction et de l'étape de marche
         image = self.images[self.direction][self.walk_step]
         screen.blit(image, self.rect)
+
+    def take_damage(self, damage):
+        self.hp -= damage
+        if self.hp <= 0:
+            self.hp = 0  # Le personnage a 0 points de vie
+            self.eliminated = True  # Marquez l'ennemi comme éliminé
+
+    def heal(self, amount):
+        self.hp += amount
+        if self.hp > self.max_hp:
+            self.hp = self.max_hp  # Assurez-vous que les HP ne dépassent pas la valeur maximale
+
+    def get_hp(self):
+        return self.hp
+
+    def get_max_hp(self):
+        return self.max_hp
+
+    def is_alive(self):
+        return self.hp > 0
+
+    def update(self):   #je ne l'utilise pas encore
+        # Augmentez le compteur du timer de la santé
+        self.hp_timer += 1 / 60  # Augmente d'une seconde chaque frame (60 FPS)
+
+        # Vérifiez si le timer de la santé a atteint 1 seconde
+        if self.hp_timer >= 1:
+            self.hp_timer = 0  # Réinitialisez le timer
+            self.take_damage(1)  # Faites perdre 1 HP au personnage
+
--- screens/niveau1.py
+++ screens/niveau1.py
@@ -4,6 +4,7 @@ import screens.error as error
 import pytmx
 import game_logic
 import enemy
+import screens.game_over
 
 def ouvrir_niveau(screen):
     # Définit l'horloge pour connaître le temps qui a passé
@@ -15,7 +16,6 @@ def ouvrir_niveau(screen):
 
     # Crée un personnage
     character_obj = character.Character(640, 360)  # Position initiale du personnage
-    
 
      # Chargement de la carte
     tmx_map = pytmx.load_pygame('./assets/maps/sol.tmx')
@@ -36,7 +36,6 @@ def ouvrir_niveau(screen):
     pygame.mixer.music.play()
 
     while running:
-
         screen.fill((0, 0, 0))
         if pygame.mixer.music.get_busy() == 0:  # La musique s'est terminée
                 pygame.mixer.music.load('./assets/music/combat.mp3')
@@ -54,7 +53,8 @@ def ouvrir_niveau(screen):
                 running = False
             if event.type == pygame.MOUSEBUTTONDOWN and event.button ==1:
                 mouse_x, mouse_y = event.pos
-                projectiles.append(game_logic.tirer(character_obj.get_centre_x(),character_obj.get_centre_y(),mouse_x,mouse_y,screen))
+                projectiles.append(game_logic.tirer(character_obj.get_centre_x(), character_obj.get_centre_y(), mouse_x, mouse_y, screen))
+        
         for proj in projectiles:
             proj.update()        
              
@@ -63,16 +63,23 @@ def ouvrir_niveau(screen):
                     monstre.take_damage(1)  # Chaque projectile inflige 1 point de dégât
                     if not monstre.is_alive():
                         monstres.remove(monstre)  # Supprimez l'ennemi s'il n'a plus de points de vie
-                    projectiles.remove(proj)  # Supprimez le projectile s'il touche un ennemiQZ
+                    projectiles.remove(proj)  # Supprimez le projectile s'il touche un ennemi
                     break  # Sortez de la boucle des ennemis, car le projectile a déjà touché un ennemi
             proj.draw(screen)    
 
         for monstre in monstres:
-            monstre.draw(screen)  # Dessinez le monstre à l'écran
-            #print(monstre)
+            monstre.draw(screen)  
+
+        character_rect = character_obj.get_rect()
+        for monstre in monstres:
+            if character_rect.colliderect(monstre.rect):
+                character_obj.take_damage(1)  # Le personnage perd 1 point de vie en cas de collision
+                if not character_obj.is_alive():
+                    # Le personnage est mort, vous pouvez gérer la fin du jeu ou d'autres actions appropriées
+                    running = False
+                    screens.game_over.ouvrir_game_over(screen)
 
         # Affiche le personnage
-        #character_obj.inputs(pygame.key.get_pressed())
         game_logic.move_character(character_obj, pygame.key.get_pressed(), tmx_map_data)
         character_obj.draw(screen)
         # Comme les dessins sont faits dans un buffer, permute le buffer
@@ -82,5 +89,3 @@ def ouvrir_niveau(screen):
 
     # Termine proprement le module
     pygame.quit()
-
-
