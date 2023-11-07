1c25bbb49298101defcc8944083361e7d087a54a
Chloé <chloe.despesse@etu.univ-grenoble-alpes.fr>
2023-11-07 10:42:29 +0100

    salle 39

---
+++ assets/maps/pixel-cyberpunk-interior.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.2" name="pixel-cyberpunk-interior" tilewidth="32" tileheight="32" tilecount="231" columns="21">
+ <image source="pixel-cyberpunk-interior.png" width="672" height="352"/>
+</tileset>
