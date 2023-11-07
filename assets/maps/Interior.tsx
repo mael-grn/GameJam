5a04503e228507f1a3da45c99f5b9d8575458084
Chloé <chloe.despesse@etu.univ-grenoble-alpes.fr>
2023-11-07 13:41:06 +0100

    fin modification étage 1

--- assets/maps/Interior.tsx
+++ assets/maps/Interior.tsx
@@ -1,5 +1,15 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Interior" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
- <grid orientation="orthogonal" width="64" height="32"/>
+<tileset version="1.8" tiledversion="1.8.2" name="Interior" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
  <image source="Interior.png" width="1024" height="1024"/>
+ <tile id="756">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32.1053" y="31.8421">
+    <polygon points="0,0 -22.8947,0 -23.1579,-12.8947 -17.1053,-15.6579 -0.131579,-15.6579"/>
+   </object>
+   <object id="2" x="9" y="16" width="23" height="16"/>
+   <object id="3" x="9" y="16" width="23" height="16"/>
+   <object id="4" x="9" y="16" width="23" height="16"/>
+   <object id="5" name="t" x="8.86842" y="16.1053" width="23.1316" height="15.8947"/>
+  </objectgroup>
+ </tile>
 </tileset>
