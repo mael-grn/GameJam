c127d8c6120d4ad6f581d29bfb615b1bfe119fb0
maelg <mael.garnier@univ-grenoble-alpes.fr>
2023-11-07 11:22:03 +0100

    correction bug chemin acces

--- assets/maps/Interior.tsx
+++ assets/maps/Interior.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.2" name="Interior" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+<tileset version="1.10" tiledversion="1.10.2" name="Interior" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
  <grid orientation="orthogonal" width="64" height="32"/>
- <image source="../../../../Downloads/Interior.png" width="1024" height="1024"/>
+ <image source="Interior.png" width="1024" height="1024"/>
 </tileset>
