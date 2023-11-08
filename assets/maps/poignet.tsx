1c9b1e2724a27ca4d5597b20f27735ee6a43f7cb
Yoann <yoann.goumarre@etu.univ-grenoble-alpes.fr>
2023-11-08 09:58:26 +0100

    petits reglages

--- assets/maps/poignet.tsx
+++ assets/maps/poignet.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.2" name="poignet" tilewidth="32" tileheight="32" tilecount="1" columns="1">
- <image source="../../../../Downloads/poignet.png" width="32" height="32"/>
+ <image source="poignet.png" width="32" height="32"/>
 </tileset>
