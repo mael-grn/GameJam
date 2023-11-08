a2c12e6d2b0ec3b64be0577ca7212943616716d1
maelg <mael.garnier@univ-grenoble-alpes.fr>
2023-11-08 15:10:20 +0100

    bug

--- salle.py
+++ salle.py
@@ -1,11 +1,12 @@
-import pyymx
+import pytmx
 import character
 import piece
 import enemy
 
 class Salle:
-    def __init__(self, tmx_file, character, enemies, pieces):
+    def __init__(self, tmx_file, character, enemies, pieces, entry):
         self.map = tmx_file  
+        self.entry = entry
         self.character = character
         self.enemies = enemies 
         self.pieces = pieces   
