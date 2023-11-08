c9bb5eabb8f2543b4c6af8650306f439acffb567
Chloé <chloe.despesse@etu.univ-grenoble-alpes.fr>
2023-11-08 09:40:44 +0100

    test collisions

--- assets/maps/Interior.tsx
+++ assets/maps/Interior.tsx
@@ -4,7 +4,84 @@
  <tile id="206">
   <objectgroup draworder="index" id="2">
    <object id="1" x="31.8923" y="0.25563" visible="0">
-    <polygon points="0,0 -17.904,-0.102054 -23.804,2.95974 -23.9575,31.6369 0.102602,31.7904"/>
+    <polygon points="0,0 -17.904,-0.102054 -23.804,2.95974 -23.9575,31.6369 -2.25222,28.8725"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="442">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.91304" y="31.9565">
+    <polygon points="0,0 -0.0434783,-10.087 24.0435,-9.95652 24.1304,0.0434783"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="471">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="5.25" y="0.125">
+    <polygon points="0,0 5.5,0 5.75,10.6875 21.5625,10.9375 22,32 -0.5,31.9375 -0.9375,-0.125"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="477">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="31.0625" y="32">
+    <polygon points="0,0 -0.1875,-21 -0.3125,-22.5 -29.5,-22.4375 -29.125,-0.1875"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="509">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.9375" y="0">
+    <polygon points="0,0 0,31.8125 28.125,32 28.0625,-0.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="541">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.875" y="0.3125">
+    <polygon points="0,0 0.125,31.5 28.1875,31.6875 28.0625,-0.375 -0.25,-0.5"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="595">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="31.8413" y="16.023">
+    <polygon points="0,0 -17.7636,0 -23.8042,2.96912 -23.8042,15.8183 -23.8042,15.9718 0.202173,15.977"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="596">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24.0435" y="32.087">
+    <polygon points="0,0 -0.0434783,-13.0435 -6.08696,-16.0435 -24,-16.0435 -24,-0.130435"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="627">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.9375" y="0.0625">
+    <polygon points="0,0 0.0625,31.9375 24.0625,32.0625 24.125,0.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="628">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="24.0625" y="0.0625">
+    <polygon points="0,0 0,31.75 -23.9375,31.9375 -23.9375,-0.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="659">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="0.0625">
+    <polygon points="0,0 -24.25,0 -23.5,31.875 0.125,31.9375"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="660">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="23.875" y="32">
+    <polygon points="0,0 0.125,-32.0625 -23.9375,-31.9375 -23.8125,-0.1875"/>
    </object>
   </objectgroup>
  </tile>
